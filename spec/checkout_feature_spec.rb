3e195bb517869a9b62afab772f6d606cc2faea3b
Minh Nguyen <minhnguyenfu@gmail.com>
2019-11-14 23:35:47 +0700

    failing percantage off promo feature test

--- spec/checkout_feature_spec.rb
+++ spec/checkout_feature_spec.rb
@@ -3,13 +3,13 @@ require_relative '../lib/item'
 
 describe 'Checkout' do
   context 'when there are no promotional rules' do
-    it '#total returns £0.00 when no items are scanned' do
+    it 'returns £0.00 when no items are scanned' do
       co = Checkout.new
 
       expect(co.total).to eq '£0.00'
     end
 
-    it '#total returns the combined total when given three items' do
+    it 'returns the combined total when given three items' do
       item1 = Item.new('001', 'Very Cheap Chair', 925)
       item2 = Item.new('002', 'Little table', 4500)
       item3 = Item.new('003', 'Funky light', 1995)
@@ -20,6 +20,22 @@ describe 'Checkout' do
       co.scan(item3)
 
       expect(co.total).to eq '£74.20'
+    end
+  end
+
+  context 'when there is a 10% percentage off total promotional rule' do
+    it 'returns the total with 10% taken off' do
+      item1 = Item.new('001', 'Very Cheap Chair', 925)
+      item2 = Item.new('002', 'Little table', 4500)
+      item3 = Item.new('003', 'Funky light', 1995)
+      promotional_rules = [{ type: 'percentage_off_basket', eligible_amount: 6000, percantage: 10 }]
+
+      co = Checkout.new(promotional_rules)
+      co.scan(item1)
+      co.scan(item2)
+      co.scan(item3)
+
+      expect(co.total).to eq '£66.78'
     end
   end
 end
